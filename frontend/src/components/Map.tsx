da4db30710c9a2a56c56f5d3f8d17cd924d26245
mikha <mikhaelmiro300@gmail.com>
2025-01-19 13:38:19 +0100

    fixing map

--- frontend/src/components/Map.tsx
+++ frontend/src/components/Map.tsx
@@ -49,7 +49,10 @@ export default function Map({ departure, arrival }: { departure: string; arrival
   return (
     <MapContainer center={[20, 0]} zoom={3} style={{ height: "400px", width: "100%" }}>
       {/* Tile Layer for map background */}
-      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      <TileLayer
+  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+/>
       
       {/* Draw a smooth curved line from departure to arrival */}
       {route.length > 0 && <Polyline positions={route} color="red" weight={3} smoothFactor={1.5} />}
