3dc1ef16a0899f739696089a4c12e8379384270c
mikha <mikhaelmiro300@gmail.com>
2025-02-03 11:36:07 +0100

    migrated products to another repo for now

--- products/mrc/build/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_MRC_FILE C:/Users/mikha/Desktop/Project/SkyForge/products/mrc/build/MRC.exe)
-set(__QT_DEPLOY_TARGET_MRC_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_MRC_RUNTIME_DLLS M:/Qt/6.8.1/mingw_64/bin/Qt6Widgets.dll;M:/Qt/6.8.1/mingw_64/bin/Qt6Gui.dll;M:/Qt/6.8.1/mingw_64/bin/Qt6Core.dll)
